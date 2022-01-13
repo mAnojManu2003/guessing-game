86610985589e853732eb9ba74593e2b61f92c321
mAnojManu2003 <95956060+mAnojManu2003@users.noreply.github.com>
2022-01-13 21:48:38 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+     int num1,num2,num3,num4,num5,num6;
+     printf("\nGuess any number : ");
+scanf("%s",&num1);
+     printf("\nTake the same number with your friend and add both : ");
+     scanf("%d",&num2);
+     printf("\nI will give you 100 add this too : ");
+     scanf("%s",&num3);
+     printf("\nGive half of the total to the god in the total: ");
+     scanf("%s",&num4);
+     printf("\nReturn the same number to your friend which you have taken in the starting : ");
+     scanf("%s",&num5);
+
+     printf("\n AT finall you have 50 remaining ");
+     printf("\n***********THANK YOU FOR PARTICIPATING IN  THIS**************\a");
+     return 0;
+}
+
+
+
+
+
+
+
+
+
